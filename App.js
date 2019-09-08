59ffc3f1cd0a8898af5ca3a9d05b18ab73ba5c17
shashank.verma <shashank.verma>
2019-09-08 15:44:45 -0400

    added app.js

---
+++ App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import PlacesNavigator from "./navigation/PlacesNavigation";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import placesReducer from './store/reducers/places';
+
+const rootReducer = combineReducers({
+  places: placesReducer
+});
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+export default function App() {
+  return (
+    <Provider store = {store}>
+      <PlacesNavigator /> 
+    </Provider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
